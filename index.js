1a188efa14255eaab3ecc24f546e7b4458cd7302
Jesse <jesse9726@gmail.com>
2018-03-21 22:50:25 -0700

    updated welcome message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req,res){
-    res.send('Hello World from paypal');
+    res.send('Hello World from test server');
 });
 
 app.post('/create-payment1/', async function(req,res){
